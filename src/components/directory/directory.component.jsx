6402846be851565558135c49044b400f5f3274b6
Артем Баторшин <norvellart@gmail.com>
2021-09-13 10:33:37 +0300

    add withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
-import './directory.styles.scss'
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
@@ -12,7 +12,7 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "jackets",
@@ -44,20 +44,14 @@ class Directory extends React.Component {
     };
   }
 
-  render(){
-      return(
-        <div className="directory-menu">
-            {
-            this.state.sections.map(({title, imageUrl, id, size}) =>(
-                <MenuItem 
-                key={id} 
-                title={title} 
-                imageUrl={imageUrl}
-                size={size} />
-            ))
-            }
-        </div>
-      )
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
